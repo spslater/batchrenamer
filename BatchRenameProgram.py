7a68623e806f9dd87e7bbd8886d8c9f47ed47975
Sean Slater <sean@spslater.com>
2021-02-01 02:34:28 -0500

    Add option for automated script

--- BatchRenameProgram.py
+++ BatchRenameProgram.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from csv import reader
 from os import path, rename
+from os.path import join
 from sys import argv
-import logging as log
 
+import logging
 import re
 try:
 	import readline
@@ -11,7 +14,7 @@ except:
 	print("Readline not available")
 
 
-log.basicConfig(format='[%(levelname)s]\t%(message)s', level=log.INFO)
+logging.basicConfig(format='[%(levelname)s]\t%(message)s', level=logging.INFO)
 
 CONFIM = ["y", "Y", "yes", "Yes"]
 DENY = ["n", "N", "no", "No"]
@@ -26,55 +29,59 @@ class Renamer:
 		self.actions = [
 			{ "opts": ["q", "quit"],
 				"help_opt": "q[uit]",
-				"message": "Quit program, don't apply unsaved changes.", 
+				"message": "Quit program, don't apply unsaved changes.",
 				"method": self.quit },
 			{ "opts": ["h", "help"],
 				"help_opt": "h[elp]",
-				"message": "Print help message.", 
+				"message": "Print help message.",
 				"method": self.help },
 			{ "opts": ["u", "undo"],
 				"help_opt": "u[ndo]",
-				"message": "Undo last regex applied.", 
+				"message": "Undo last regex applied.",
 				"method": self.undo },
 			{ "opts": ["s", "save"],
 				"help_opt": "s[ave]",
-				"message": "Save files with current changes.", 
+				"message": "Save files with current changes.",
 				"method": self.save },
 			{ "opts": ["r", "regex"],
 				"help_opt": "r[egex]",
-				"message": "Find and replace based on a regex.", 
+				"message": "Find and replace based on a regex.",
 				"method": self.findAndReplace },
 			{ "opts": ["l", "list"],
 				"help_opt": "l[ist]",
-				"message": "Lists current files being modified.", 
+				"message": "Lists current files being modified.",
 				"method": self.printFileChanges },
 			{ "opts": ["e", "ext"],
 				"help_opt": "e[xt]",
-				"message": "Change all the extensions on all files.", 
+				"message": "Change all the extensions on all files.",
 				"method": self.changeExt },
 			{ "opts": ["i", "insert"],
 				"help_opt": "i[nsert]",
-				"message": "Insert string, positive from begining, negative from ending", 
+				"message": "Insert string, positive from begining, negative from ending",
 				"method": self.insertString },
 			{ "opts": ["t", "track"],
 				"help_opt": "t[rack]",
-				"message": "Load tsv file with track number and song titles.", 
+				"message": "Load tsv file with track number and song titles.",
 				"method": self.prependTrackNumbers },
 			{ "opts": ["n", "name"],
 				"help_opt": "n[ame]",
-				"message": "Load tsv file with episode number and titles.", 
+				"message": "Load tsv file with episode number and titles.",
 				"method": self.appendEpisodeNames },
 			{ "opts": ["w", "write"],
 				"help_opt": "w[rite]",
-				"message": "Write changes and quit program, same as save then quit.", 
+				"message": "Write changes and quit program, same as save then quit.",
 				"method": self.saveAndQuit },
+			{ "opts": ["a", "auto"],
+				"help_opt": "a[uto]",
+				"message": "Automate some commands in order to speed up repetative tasks.",
+				"method": self.automate },
 		]
 		self.files = []
-		for name in names: 
+		for name in names:
 			self.files.append(FileRename(name))
 
 	def _todo(self):
-		log.warn("NEED TO IMPLAMENT")
+		logging.warn("NEED TO IMPLAMENT")
 
 	def _calcHelpPadding(self):
 		maxLen = 0
@@ -102,11 +109,11 @@ class Renamer:
 			print("\t" + action["help_opt"].ljust(help_pad) + " : " + action["message"])
 		print()
 
-	def save(self, r=None):
-		really = userInput("Are you sure you want to save new names? ") if r is None else r
+	def save(self, really_in=None):
+		really = userInput("Are you sure you want to save new names? ") if really_in is None else really_in
 		while True:
-			if really in CONFIM: 
-				for f in self.files: 
+			if really in CONFIM:
+				for f in self.files:
 					f.save()
 				print("Files renamed.")
 				break
@@ -116,10 +123,10 @@ class Renamer:
 			else:
 				really = userInput("Yes or No? ")
 
-	def quit(self, r=None):
-		really = userInput("Are you sure you want to quit? ") if r is None else r
+	def quit(self, really_in=None):
+		really = userInput("Are you sure you want to quit? ") if really_in is None else really_in
 		while True:
-			if really in CONFIM: 
+			if really in CONFIM:
 				print("Thanks for using!")
 				exit()
 			elif really in DENY:
@@ -127,10 +134,10 @@ class Renamer:
 			else:
 				really = userInput("Yes or No? ")
 
-	def saveAndQuit(self):
-		really = userInput("Are you sure you want to save and quit? ")
+	def saveAndQuit(self, really_in=None):
+		really = userInput("Are you sure you want to save and quit? ") if really_in is None else really_in
 		while True:
-			if really in CONFIM: 
+			if really in CONFIM:
 				self.save("yes")
 				self.quit("yes")
 				exit()
@@ -141,73 +148,89 @@ class Renamer:
 
 	def undo(self):
 		undoneAll = True
-		for f in self.files: 
+		for f in self.files:
 			undoneAll = f.undo() and True
 		print(("Last " if undoneAll else "All ") + "change" + (" has" if undoneAll else "s have") + " been undone.")
 
-	def findAndReplace(self, f=None, r=None):
-		find = input("Find: ") if f is None else f
-		repl = input("Repl: ") if r is None else r
+	def findAndReplace(self, find_in=None, replace_in=None):
+		find = input("Find: ") if find_in is None else find_in
+		repl = input("Repl: ") if replace_in is None else replace_in
 		for f in self.files:
 			f.replace(find, repl)
 		self.printFileChanges()
 
-	def changeExt(self):
-		repl = input("New Ext: ")
+	def changeExt(self, repl_in=None):
+		repl = input("New Ext: ") if repl_in is None else repl_in
 		for f in self.files:
 			f.ext = repl if repl[0] == "." else "." + repl
 
-	def insertString(self):
-		val = input("Insert: ")
+	def insertString(self, val_in=None, num_in=None, good_in=None):
+		val = input("Insert: ") if val_in is None else val_in
 		num = 0
 		while True:
 			try:
-				num = int(userInput("Index: "))
+				num = int(userInput("Index: ")) if num_in is None else int(num_in)
 				orig = test = self.files[0].rename
 				if num > 0:
 					if num < len(test):
 						idx = num
 					else:
 						idx = len(test)
-					find = "^(.{"+str(idx)+"})(.*)"
+					find = "^(.{{{}}})(.*)".format(idx)
 				elif num <= 0:
 					if num > -1 * len(test):
 						idx = -1 * num
 					else:
 						idx = 0
-					find = "(.*?)(.{"+str(idx)+"})$"
+					find = "(.*?)(.{{{}}})$".format(idx)
 				repl = r'\1' + val + r'\2'
 				test = re.sub(find, repl, test)
 				print("Example:\n" + test)
-				good = userInput("Right index? ")
+				good = userInput("Right index? ") if good_in is None else good_in
 				if good in CONFIM: break
 				if good in BACK: return
 			except ValueError:
 				print("Please enter an int.")
 		self.findAndReplace(find, repl)
 
-	def appendEpisodeNames(self):
-		tsv = input("Filepath: ")
-		with open(tsv) as f:
-			for ep in f:
-				num = ep.split('\t')[0].strip()
-				title = ep.split('\t')[1].strip()
+	def appendEpisodeNames(self, tsv_in=None):
+		tsv = input("Filepath: ") if tsv_in is None else tsv_in
+		with open(tsv, newline='') as tp:
+			tsv_reader = reader(tp, delimiter='\t')
+			for ep in tsv_reader:
+				num = ep[0].strip()
+				title = ep[1].strip()
 				for f in self.files:
-					if re.search("- " + str(num) + " -", f.rename) is not None:
+					if re.search("- {} -".format(num), f.rename) is not None:
 						f.replace('$', title)
 						break
 		self.printFileChanges()
 
-	def prependTrackNumbers(self):
-		tsv = input("Filepath: ")
-		with open(tsv) as f:
-			for track in f:
-				num = track.split('\t')[0].strip()
-				title = track.split('\t')[1].strip()
+	def prependTrackNumbers(self, tsv_in=None):
+		tsv = input("Filepath: ") if tsv_in is None else tsv_in
+		with open(tsv) as tp:
+			tsv_reader = reader(tp, delimiter='\t')
+			for track in tsv_reader:
+				num = track[0].strip()
+				title = track[1].strip()
 				for f in self.files:
-					if re.search("^" + re.escape(title) + "$", f.rename) is not None:
-						f.replace('^', str(num) + " ")
+					if re.search("^{}$".format(re.escape(title)), f.rename) is not None:
+						f.replace('^', "{} ".format(num))
 						break
+		self.printFileChanges()
+
+	def automate(self, tsv_in=None):
+		tsv = input("Filepath: ") if tsv_in is None else tsv_in
+		with open(tsv) as tp:
+			tsv_reader = reader(tp, delimiter='\t')
+			for command in tsv_reader:
+				opt = command[0]
+				args = command[1:]
+				for act in self.actions:
+					if opt in act["opts"]:
+						act["method"](*args)
+						break
+
 		self.printFileChanges()
 
 
@@ -223,10 +246,10 @@ class FileRename:
 		self.rename = self.previous
 
 	def __str__(self):
-		return "\n\tFull: " + self.full + "\n\tRename: " + self.rename
+		return "\n\tFull: {}\n\tRename: {}".format(self.full, self.rename)
 
 	def fullname(self):
-		return self.directory + "/" + self.rename + self.ext
+		return join(self.directory, self.rename + self.ext)
 
 	def replace(self, find, repl):
 		self.nameList.append(self.previous)
@@ -246,21 +269,18 @@ class FileRename:
 		self.name = self.rename
 
 	def move(self):
-		old = self.directory + "/" + self.name   + self.ext
-		new = self.directory + "/" + self.rename + self.ext
+		old = join(self.directory,   self.name + self.ext)
+		new = join(self.directory, self.rename + self.ext)
 		rename(old, new)
 
 	def printChanges(self):
 		print(self.name + self.ext + "\n" + self.rename + self.ext + "\n")
 
 
-def main():
-	if len(argv) == 1:
-		print("Usage: rename <file> ...")
-		exit()
-
-	Renamer(argv[1:]).run()
-
-
 if __name__ == "__main__":
-	main()
+	parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+	parser.add_argument('files', nargs='+',
+		help="Files to be renamed.")
+
+	args = parser.parse_args()
+	Renamer(args.files).run()
